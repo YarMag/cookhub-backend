0e1719932b8ab8ecc849229c39e142013dca0d86
Yaroslav Magin <magin.yaroslav@gmail.com>
2022-01-02 14:47:19 +0300

    Updated migration.

--- db/migrations/000001_create_onboarding_table.up.sql
+++ db/migrations/000001_create_onboarding_table.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS cookhubdb.onboarding
 (
 	onboarding_id INT PRIMARY KEY,
-	title VARCHAR (50) NOT NULL,
+	title VARCHAR (100) NOT NULL,
 	image_url VARCHAR(300) NOT NULL
 );
